ecaa095cac0bd3acfea53cc7d1e1a19b8a0399b1
dongree <dongwoo0307@naver.com>
2024-05-16 21:07:05 +0900

    Feat: 로그인, 로그아웃 구현 with NextAuth

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,20 @@
+'use client';
+
+import { useSession, signIn, signOut } from 'next-auth/react';
+
 export default function Header() {
+  const { data: session } = useSession();
+
   return (
     <header className="flex justify-between items-center px-10 bg-sky-300 h-16">
       <h1>Brush Up</h1>
       <div className="flex gap-5">
         <div>입력</div>
-        <button>로그인</button>
+        {session ? (
+          <button onClick={() => signOut()}>로그아웃</button>
+        ) : (
+          <button onClick={() => signIn()}>로그인</button>
+        )}
       </div>
     </header>
   );
