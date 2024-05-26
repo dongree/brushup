3182b1e54e8483f38e8e0c5bd6658a2fbbe6411f
dongree <dongwoo0307@naver.com>
2024-05-27 00:33:49 +0900

    Chore: 간단한 UI 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="mt-[5%] flex flex-col items-center justify-between">
+    <main className="mt-32px flex flex-col items-center justify-between">
       <h3 className="my-8 text-2xl font-medium">오늘의 Brush Up 문제</h3>
       <section className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 ">
         <QuestionCard />
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,13 +2,16 @@
 
 import { useSession, signIn, signOut } from "next-auth/react";
 import { Button } from "./ui/button";
+import Link from "next/link";
 
 export default function Header() {
   const { data: session } = useSession();
 
   return (
-    <header className="flex h-16 items-center justify-between bg-sky-300 px-10">
-      <h1>Brush Up</h1>
+    <header className="flex h-16 items-center justify-between bg-gray-100 px-10">
+      <h1 className="text-lg font-semibold">
+        <Link href={"/"}> Brush Up</Link>
+      </h1>
       <div className="flex gap-5">
         <Button>입력</Button>
         {session ? (
