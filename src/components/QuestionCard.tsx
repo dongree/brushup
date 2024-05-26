7ea89f46d8a24eb25086fc8c7f1464e9c04e8569
dongree <dongwoo0307@naver.com>
2024-05-27 00:28:28 +0900

    Fix: Card UI 수정 with shadcn ui

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,20 +1,34 @@
 "use client";
 
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Button } from "./ui/button";
+
 export default function QuestionCard() {
   return (
-    <section className="flex h-48 w-48 flex-col items-center justify-between rounded-lg border-[1px] border-black bg-white py-1 text-sm">
-      <button className="w-full border-b-[1px] border-black">
-        문제 보러 가기
-      </button>
-      <div>XXO</div>
-      <div>난이도 : HARD</div>
-      <div>푼 횟수 : 2</div>
-      <div>Hint 보기</div>
-      <div className="flex w-full justify-around border-t-[1px] border-black">
-        <button>해결</button>
-        <button>살패</button>
-        <button>나중에</button>
-      </div>
-    </section>
+    <Card className="flex flex-col items-center">
+      <CardHeader>
+        <CardTitle className="text-base">
+          수 합치기2 <button>{">"}</button>
+        </CardTitle>
+      </CardHeader>
+      <CardContent className="flex flex-col items-center">
+        <div>난이도 : HARD</div>
+        <div>history : XXO</div>
+        <div>시도 횟수 : 2</div>
+        <div>최근 푼 날짜 : 2024.05.20</div>
+        <div>Hint 보기</div>
+      </CardContent>
+      <CardFooter className="gap-1">
+        <Button>해결</Button>
+        <Button>살패</Button>
+        <Button>나중에</Button>
+      </CardFooter>
+    </Card>
   );
 }
